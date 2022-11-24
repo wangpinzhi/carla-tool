6c6b4e4f46c0eefc975023167418e5a249dfcbd7
MiniTITAN <cdwpz302@outlook.com>
2022-11-24 20:52:01 +0800

    更新Town02 data采集

--- scripts/collect_data.bat
+++ scripts/collect_data.bat
@@ -1,5 +1,5 @@
 @echo off
 
-start python tools/sim_run.py  --frames 1000 --fixed_delta_time 0.08  --save_data_path "Town01_1000_frames" 
+start python tools/sim_run.py  --frames 500 --fixed_delta_time 0.08  --save_data_path "Town02_500_frames" 
 
 pause
