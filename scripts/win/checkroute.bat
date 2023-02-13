10267ce82380d25bc8cf2e03d7bb37d07724b743
wangpinzhi <cdwpz302@outlook.com>
2023-02-13 11:57:04 +0800

    update

--- scripts/win/checkroute.bat
+++ scripts/win/checkroute.bat
@@ -1,7 +1,9 @@
 @echo off
 
 @REM Variables
+set VERSION=%1
+set SCENE=%2
 
-python tools/sim_checkroute.py  --config_path "output/huawei_parking07/checkroute.json"
+python tools/sim_checkroute.py  --config_path "output/%VERSION%_%SCENE%/%VERSION%_%SCENE%_part1.json"
                 
 pause
