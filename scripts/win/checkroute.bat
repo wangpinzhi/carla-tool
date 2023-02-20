27375f7360efbbc744e7aa2be17bc4d7af0df6e1
wangpinzhi <cdwpz302@outlook.com>
2023-02-20 14:40:23 +0800

    初始化新分支

--- scripts/win/checkroute.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-@REM Variables
-set VERSION=%1
-set SCENE=%2
-
-python tools/sim_checkroute.py  --config_path "output/%VERSION%_%SCENE%/%VERSION%_%SCENE%_part1.json"
-                
-pause
