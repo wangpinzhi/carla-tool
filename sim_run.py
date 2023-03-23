1b45c6880bc30934782600dcd9c0c01eb2c499f8
wangpinzhi <cdwpz302@outlook.com>
2023-03-23 10:31:13 +0800

    complete useful_tools

--- sim_run.py
+++ sim_run.py
@@ -8,12 +8,13 @@ if __name__ == '__main__':
     #     parameter_path_scene=r'output\huawei_demo_parking\configs\scene_config.json',
     #     parameter_path_save=r'output\huawei_demo_parking\raw_data',
     # )
+    name = 'robot_vaccum_scene1'
     local_val_simulator_manager = ClassSimulatorManager(
         parameter_host='127.0.0.1',
         parameter_port=2000,
-        parameter_path_sensor=r'output\huawei_parking21\configs\sensor_config.json',
-        parameter_path_scene=r'output\huawei_parking21\configs\scene_config.json',
-        parameter_path_save=r'output\huawei_parking21\raw_data',
+        parameter_path_sensor=rf'output\{name}\configs\sensor_config.json',
+        parameter_path_scene=rf'output\{name}\configs\scene_config.json',
+        parameter_path_save=rf'output\{name}\raw_data',
     )
     local_val_simulator_manager.function_init_world()
-    local_val_simulator_manager.function_start_sim_collect(parameter_split_num=4)
+    local_val_simulator_manager.function_start_sim_collect(parameter_split_num=1)
