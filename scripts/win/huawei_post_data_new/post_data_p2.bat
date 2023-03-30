4365431b9a606f30e7f74c31e1a9f44da0d69c56
wangpinzhi <cdwpz302@outlook.com>
2023-03-30 15:25:34 +0800

    add new scripts

--- scripts/win/huawei_post_data_new/post_data_p2.bat
+++ scripts/win/huawei_post_data_new/post_data_p2.bat
@@ -5,36 +5,36 @@ set ROOT_PATH=%1
 
 echo "process cm_rgb9 cm_rgb10 cm_rgb12"
 
-@REM start python tools/cubemap2fisheye.py   --fov 190^
-@REM                                         --cubeW 2560^
-@REM                                         --outW 2560^
-@REM                                         --r_x 15^
-@REM                                         --external_path "%ROOT_PATH%/external.txt"^
-@REM                                         --cubemap_dir "%ROOT_PATH%/cubemap"^
-@REM                                         --output_dir "%ROOT_PATH%/fisheye190_new"^
-@REM                                         --camera "cm_rgb9"^
-@REM                                         --use_cuda
+python tools/cubemap2fisheye.py   --fov 190^
+                                        --cubeW 2560^
+                                        --outW 2560^
+                                        --r_x 15^
+                                        --external_path "%ROOT_PATH%/external.txt"^
+                                        --cubemap_dir "%ROOT_PATH%/cubemap"^
+                                        --output_dir "%ROOT_PATH%/fisheye190_new"^
+                                        --camera "cm_rgb9"^
+                                        --use_cuda
                                         
 
-@REM start python tools/cubemap2fisheye.py   --fov 190^
-@REM                                         --cubeW 2560^
-@REM                                         --outW 2560^
-@REM                                         --r_x 15^
-@REM                                         --external_path "%ROOT_PATH%/external.txt"^
-@REM                                         --cubemap_dir "%ROOT_PATH%/cubemap"^
-@REM                                         --output_dir "%ROOT_PATH%/fisheye190_new"^
-@REM                                         --camera "cm_rgb10"^
-@REM                                         --use_cuda
+python tools/cubemap2fisheye.py   --fov 190^
+                                        --cubeW 2560^
+                                        --outW 2560^
+                                        --r_x 15^
+                                        --external_path "%ROOT_PATH%/external.txt"^
+                                        --cubemap_dir "%ROOT_PATH%/cubemap"^
+                                        --output_dir "%ROOT_PATH%/fisheye190_new"^
+                                        --camera "cm_rgb10"^
+                                        --use_cuda
                                         
 
-@REM start /WAIT python tools/cubemap2fisheye.py   --fov 190^
-@REM                                         --cubeW 2560^
-@REM                                         --outW 2560^
-@REM                                         --r_x 15^
-@REM                                         --external_path "%ROOT_PATH%/external.txt"^
-@REM                                         --cubemap_dir "%ROOT_PATH%/cubemap"^
-@REM                                         --output_dir "%ROOT_PATH%/fisheye190_new"^
-@REM                                         --camera "cm_rgb12"^
-@REM                                         --use_cuda
+python tools/cubemap2fisheye.py   --fov 190^
+                                        --cubeW 2560^
+                                        --outW 2560^
+                                        --r_x 15^
+                                        --external_path "%ROOT_PATH%/external.txt"^
+                                        --cubemap_dir "%ROOT_PATH%/cubemap"^
+                                        --output_dir "%ROOT_PATH%/fisheye190_new"^
+                                        --camera "cm_rgb12"^
+                                        --use_cuda
                                         
 
--- scripts/win/huawei_post_data_new/post_data_p3.bat
+++ scripts/win/huawei_post_data_new/post_data_p3.bat
@@ -5,7 +5,7 @@ set ROOT_PATH=%1
 
 echo "process cm_rgb4 cm_rgb5 cm_rgb2"
 
-start python tools/cubemap2pinhole.py   --fov 150^
+python tools/cubemap2pinhole.py   --fov 150^
                                         --cubeW 2560^
                                         --outH 1856^
                                         --outW 2880^
@@ -17,26 +17,26 @@ start python tools/cubemap2pinhole.py   --fov 150^
                                         --use_cuda
                                         
 
-@REM start python tools/cubemap2pinhole.py   --fov 150^
-@REM                                         --cubeW 2560^
-@REM                                         --outH 1080^
-@REM                                         --outW 1920^
-@REM                                         --format "jpg"^
-@REM                                         --external_path "%ROOT_PATH%/external.txt"^
-@REM                                         --cubemap_dir "%ROOT_PATH%/cubemap"^
-@REM                                         --output_dir "%ROOT_PATH%/pinhole_new"^
-@REM                                         --camera "cm_rgb5"^
-@REM                                         --use_cuda
+python tools/cubemap2pinhole.py   --fov 150^
+                                        --cubeW 2560^
+                                        --outH 1080^
+                                        --outW 1920^
+                                        --format "jpg"^
+                                        --external_path "%ROOT_PATH%/external.txt"^
+                                        --cubemap_dir "%ROOT_PATH%/cubemap"^
+                                        --output_dir "%ROOT_PATH%/pinhole_new"^
+                                        --camera "cm_rgb5"^
+                                        --use_cuda
                                         
 
-@REM start /WAIT python tools/cubemap2pinhole.py   --fov 60^
-@REM                                         --cubeW 2560^
-@REM                                         --outH 1856^
-@REM                                         --outW 2880^
-@REM                                         --format "jpg"^
-@REM                                         --external_path "%ROOT_PATH%/external.txt"^
-@REM                                         --cubemap_dir "%ROOT_PATH%/cubemap"^
-@REM                                         --output_dir "%ROOT_PATH%/pinhole_new"^
-@REM                                         --camera "cm_rgb2"^
-@REM                                         --use_cuda
+python tools/cubemap2pinhole.py   --fov 60^
+                                        --cubeW 2560^
+                                        --outH 1856^
+                                        --outW 2880^
+                                        --format "jpg"^
+                                        --external_path "%ROOT_PATH%/external.txt"^
+                                        --cubemap_dir "%ROOT_PATH%/cubemap"^
+                                        --output_dir "%ROOT_PATH%/pinhole_new"^
+                                        --camera "cm_rgb2"^
+                                        --use_cuda
                                         
