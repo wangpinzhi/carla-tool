0af4e43ba280184b2259296e06e65ff1631f1d92
wangpinzhi <cdwpz302@outlook.com>
2023-04-06 14:07:41 +0800

    add EnumCubemap

--- post_process.bat
+++ post_process.bat
@@ -1,5 +1,5 @@
 @echo off
-python post_process.py  --num_workers 4^
+python post_process.py  --num_workers 2^
                         --gpu 0^
                         --batch_size 4^
                         --sensor_config_json "output/huawei_parking16/configs/sensor_config.json"^
